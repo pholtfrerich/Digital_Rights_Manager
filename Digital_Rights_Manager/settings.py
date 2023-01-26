e24d807424b232824a8ded46aaaf6256f9e4f577
Paul Holtfrerich <pholtfrerich@gmail.com>
2023-01-26 10:00:47 -0500

    first dev commit

--- Digital_Rights_Manager/settings.py
+++ Digital_Rights_Manager/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+#Dev
+
 from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
